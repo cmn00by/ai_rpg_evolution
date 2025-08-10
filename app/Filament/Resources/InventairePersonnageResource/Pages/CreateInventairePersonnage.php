48db43ed5042587ba15f552f82933051f45610e1
cmn00by <cmn00by@example.com>
2025-08-10 19:30:46 +0200

    Fix: Résoudre l'erreur 'inventaire_id doesn't have a default value' dans Filament
    - Ajouter la méthode mutateFormDataBeforeCreate() dans CreateInventairePersonnage
    - Définir automatiquement inventaire_id basé sur le personnage sélectionné
    - Mapper les noms de champs du formulaire aux colonnes de la base de données
    - Corriger la création d'InventairePersonnage via l'interface admin Filament

--- app/Filament/Resources/InventairePersonnageResource/Pages/CreateInventairePersonnage.php
+++ app/Filament/Resources/InventairePersonnageResource/Pages/CreateInventairePersonnage.php
@@ -3,10 +3,36 @@
 namespace App\Filament\Resources\InventairePersonnageResource\Pages;
 
 use App\Filament\Resources\InventairePersonnageResource;
+use App\Models\Personnage;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateInventairePersonnage extends CreateRecord
 {
     protected static string $resource = InventairePersonnageResource::class;
+    
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        // Récupérer l'inventaire_id du personnage sélectionné
+        if (isset($data['personnage_id'])) {
+            $personnage = Personnage::find($data['personnage_id']);
+            if ($personnage && $personnage->inventaire) {
+                $data['inventaire_id'] = $personnage->inventaire->id;
+            }
+        }
+        
+        // Renommer 'quantite' en 'quantity' pour correspondre au modèle
+        if (isset($data['quantite'])) {
+            $data['quantity'] = $data['quantite'];
+            unset($data['quantite']);
+        }
+        
+        // Renommer 'durability_current' en 'durability' pour correspondre au modèle
+        if (isset($data['durability_current'])) {
+            $data['durability'] = $data['durability_current'];
+            unset($data['durability_current']);
+        }
+        
+        return $data;
+    }
 }
